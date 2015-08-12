7802b6b523e9d2965359d28390d6360c7267d9e1
Pham Duy Dong <dongpd@exoplatform.com>
2015-08-12 08:54:47 +0700

    Store meetingpoint id for popup

--- application/src/main/webapp/js/chat.js
+++ application/src/main/webapp/js/chat.js
@@ -2842,6 +2842,7 @@ ChatApplication.prototype.displayVideoCallOnChatApp = function () {
       jzStoreParam("room", chatApplication.room);
       jzStoreParam("targetFullname", targetFullname);
       jzStoreParam("targetUser", targetUser);
+      jzStoreParam("meetingPointId", weemoExtension.meetingPointId);
       weemoExtension.showVideoPopup('/portal/intranet/videocallpopup?mode=attendee&isSpace=' + isSpace + "&spaceOrTeamName=" + spaceOrTeamName);
       //weemoExtension.joinWeemoCall(chatApplication.targetUser, chatApplication.targetFullname, chatMessage);
     }
